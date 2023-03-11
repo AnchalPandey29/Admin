2b8dc9905a731ac48813824bf199b598b38aa26c
m-triple-m <triplem656@mail.com>
2023-03-11 07:46:44 +0530

    ..

--- src/pages/dashboard/UserDataGrid.js
+++ src/pages/dashboard/UserDataGrid.js
@@ -1,6 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridToolbarContainer,
+  GridToolbarColumnsButton,
+  GridToolbarFilterButton,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+} from '@mui/x-data-grid';
 // import { useDemoData } from '@mui/x-data-grid-generator';
+
+const CustomToolbar =  () => {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarColumnsButton />
+      <GridToolbarFilterButton />
+      <GridToolbarDensitySelector />
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
 
 const UserDataGrid = () => {
 
@@ -12,6 +30,29 @@ const UserDataGrid = () => {
     //   });
     //   console.log(data);
 
+    const columns = [
+          { field: "_id", headerName: "ID", width: 150 },
+          { field: "email", headerName: "Email Address", width: 150 },
+          { field: "name", headerName: "Name", width: 200 },
+          { field: "city", headerName: "City", width: 200 },
+          { field: "tel", headerName: "Contact", width: 200 },
+      //     {
+      //       field: "email",
+      //       headerName: "Email",
+      //       width: 250,
+      //     },
+      //     {
+      //       field: "password",
+      //       headerName: "Password",
+      //       width: 150,
+      //     },
+      //     {
+      //       field: "profile",
+      //       headerName: "View Profile",
+      //       width: 130,
+      //     },
+        ];
+
     const getBlogFromBackend = async () => {
         // send request 
         const res= await fetch('http://localhost:5000/startup/getall');
@@ -19,24 +60,35 @@ const UserDataGrid = () => {
         // accessing data from response
         const data = await res.json();
 
-        console.log(data);
-        setUserList(data.result.map(obj => obj.role === 'startup'));
+        console.log(data.result.filter(obj => obj.role === 'startup'));
+        setUserList(data.result.filter(obj => obj.role === 'startup'));
 
     };
     useEffect(() => {
         getBlogFromBackend();
       }, [])
 
+      const handleRowSelection = (e) => {
+        console.log(e);
+      }
+
   return (
+    <div style={{height: '20rem'}}>
+
     <DataGrid
         // {...userList}
-        rows={userList}
-        getRowId={obj => console.log(obj)}
-        columns = {['name', 'email']}
+        rows={userList.slice(2)}
+        columns = {columns}
+        pagination
+        getRowId={obj => obj._id}
         slots={{
-          toolbar: GridToolbar,
+          toolbar: CustomToolbar,
         }}
-      />
+        checkboxSelection
+        onRowSelected={handleRowSelection}
+        
+        />
+        </div>
   )
 }
 
